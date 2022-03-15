c1958215d5e605b9f0d4e3ab6c371b400dd2ed79
jordiSDBL <jordisdbl@gmail.com>
2022-03-15 12:14:20 +0100

    Add files via upload
    Exercici Nivell 1

---
+++ S04T01N01SedoBulpeJordi/S04T01N01SedoBulpeJordi/src/main/java/cat/itacademy/barcelonactiva/sedo/bulpe/jordi/s04/t01/n01/S04T01N01SedoBulpeJordiApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.sedo.bulpe.jordi.s04.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01SedoBulpeJordiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01SedoBulpeJordiApplication.class, args);
+	}
+
+}
