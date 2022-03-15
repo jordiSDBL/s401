876aa27e81cef319775bcc2627079dd5920f41b4
jordiSDBL <jordisdbl@gmail.com>
2022-03-15 12:15:39 +0100

    Add files via upload
    Exercici Nivell 2

---
+++ S04T01N02SedoBulpeJordi/S04T01N02SedoBulpeJordi/src/main/java/cat/itacademy/barcelonactiva/sedo/bulpe/jordi/s04/t01/n02/controllers/HelloWorldController.java
@@ -0,0 +1,28 @@
+package cat.itacademy.barcelonactiva.sedo.bulpe.jordi.s04.t01.n02.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/HelloWorld")
+	public String saluda(
+			@RequestParam(name="name", required=false, defaultValue="UNKNOWN")
+			String nom
+			) {	
+		return "Hola, " + nom + ". Estàs executant un projecte Gradle.";
+	}
+		
+	@GetMapping(value = {"/HelloWorld2", "/HelloWorld2/{nom}"})
+	public String saluda2(
+			@PathVariable(required = false) 
+			String nom
+			) {
+		
+		nom = (nom != null)?nom:"UNKNOWN";
+		return "Hola, " + nom + ". Estàs executant un projecte Gradle.";    
+	}
+}
